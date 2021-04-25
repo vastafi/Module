7d73ced1acc92cee7451d3dcda6bdac059ac6049
abotnaru <alexandru.botnaru@isa.utm.md>
2021-04-26 01:15:36 +0300

    Registration page implementet, improved navbar, LogOut working correctly for the registration page

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -99,7 +99,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        return new RedirectResponse($this->urlGenerator->generate('product_index'));
 //        throw new Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
