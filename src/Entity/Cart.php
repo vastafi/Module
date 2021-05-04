03e00fd4ccef8df634148611258bbebadc66f254
Corina CIOBANU <cociobanu@pentalog.com>
2021-05-04 20:13:19 +0300

    added Cart entity

---
+++ src/Entity/Cart.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CartRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CartRepository::class)
+ */
+class Cart
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $items = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getItems(): ?array
+    {
+        return $this->items;
+    }
+
+    public function setItems(array $items): self
+    {
+        $this->items = $items;
+
+        return $this;
+    }
+}
