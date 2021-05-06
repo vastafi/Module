68d8aa9c3ed8259bda1c8489e50651bfd1e7c7d8
cociobanu <cociobanu@pentalog.com>
2021-05-06 22:37:43 +0300

    deleted drop lines from migrations

--- migrations/Version20210504180656.php
+++ migrations/Version20210504180656.php
@@ -21,13 +21,11 @@ final class Version20210504180656 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, code VARCHAR(255) NOT NULL, items JSON NOT NULL, payment_details JSON NOT NULL, status VARCHAR(255) NOT NULL, shipping_details JSON NOT NULL, total DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('DROP TABLE cart');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE cart (id INT AUTO_INCREMENT NOT NULL, items JSON NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('DROP TABLE `order`');
     }
 }
