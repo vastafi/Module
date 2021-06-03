ec48ddde6f4b809a37ef5b3fb84daff6dd3e6665
abotnaru <alexandru.botnaru@isa.utm.md>
2021-06-03 18:02:24 +0300

    in progress

---
+++ migrations/Version20210601132417.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210601132417 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE image (id INT AUTO_INCREMENT NOT NULL, tag LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\', path VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F5299398A76ED395');
+        $this->addSql('DROP INDEX IDX_F5299398A76ED395 ON `order`');
+        $this->addSql('ALTER TABLE `order` ADD code VARCHAR(255) DEFAULT NULL, DROP user_id');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE image');
+        $this->addSql('ALTER TABLE `order` ADD user_id INT DEFAULT NULL, DROP code');
+        $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F5299398A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('CREATE INDEX IDX_F5299398A76ED395 ON `order` (user_id)');
+    }
+}
