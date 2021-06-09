a47aa65fd60d14e90eed79d0279d2e45affbbc6f
Alexandr Harabara <alexandr.harabara@bitpanda.com>
2021-06-09 22:10:39 +0300

    - причёсывание миграции  - добавил token+login в api  - исправил вёрстку в списках - таблицы надо использовать только для таблиц

--- src/Security/TokenAuthenticator.php
+++ src/Security/TokenAuthenticator.php
@@ -9,9 +9,11 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Csrf\Exception\TokenNotFoundException;
@@ -20,10 +22,12 @@ use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
 class TokenAuthenticator extends AbstractGuardAuthenticator
 {
     private $em;
+    private SessionInterface $session;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, SessionInterface $session)
     {
         $this->em = $em;
+        $this->session = $session;
     }
 
     /**
@@ -42,7 +46,7 @@ class TokenAuthenticator extends AbstractGuardAuthenticator
      */
     public function getCredentials(Request $request)
     {
-        return $request->headers->get('X-AUTH-TOKEN') ?? '';
+        return $request->headers->get('X-AUTH-TOKEN') ?? $this->session->get(Security::LAST_USERNAME) ?? '';
     }
 
     public function getUser($credentials, UserProviderInterface $userProvider): ?UserInterface
